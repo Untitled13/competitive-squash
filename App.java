120f1bf67f983b9a4f2f3bef9764a9c7099355f9
Untitled13 <38769355+Untitled13@users.noreply.github.com>
2020-09-07 09:58:08 -0400

    Update App.java

--- App.java
+++ App.java
@@ -10,7 +10,6 @@ public class App {
         // System.out.println(graph.find("Carrie Fisher"));
         graph.table();
         System.out.println(graph.avgDist());
-        System.out.println(graph.topCenter().name());
         // System.out.println(graph.topCenter().name());
         // graph.recenter("Century of Cinema, A (1994)");
         // graph.table();
